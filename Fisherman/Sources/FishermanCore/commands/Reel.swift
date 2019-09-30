a467c924d94945f7a2f9f697710676fd506b2f76
Mervyn Ong <mervyn.ong@gemalto.com>
2019-09-30 16:50:37 +0800

    Begin implementing Reel

--- Fisherman/Sources/FishermanCore/commands/Reel.swift
+++ Fisherman/Sources/FishermanCore/commands/Reel.swift
@@ -1,11 +1,28 @@
 import Foundation
 import SwiftCLI
+import Yams
 
 // Reel all fishes on the manifest file, update if required. 
 public class Reel: Command {
     public func execute() throws {
         print("Reel!!!")
+        let fishermanPath = "\(FileManager.default.currentDirectoryPath)/Fisherman"
+        do {
+            let fishermanYaml = try String(contentsOfFile: fishermanPath, encoding: .utf8)
+            let decoded = try YAMLDecoder().decode(Fisherman.self, from: fishermanYaml)
+            setup(fishes: decoded.fishes)
+        } catch let error {
+            throw error
+        }
     }
+
+    private func setup(fishes: [Fish]) {
+        try! Task.run("echo", "hello")
+        for fish in fishes {
+            print("\(fish.name) \(fish.version)")
+        }
+    }
+
 }
 
 extension Reel: Routable {
